dfdb83d9b7e6d884c4b6162f416bef3fdc72782b
Faiz <faiz.ggmu@gmail.com>
2018-01-28 21:58:50 +0500

    Second Commit

---
+++ Excercises/excercise3.py
@@ -0,0 +1,92 @@
+# Excercise 3.1
+# friendlist=["Faiz","Shoaib","Farwa"]
+# print(friendlist[0])
+# print(friendlist[1])
+# print(friendlist[2])
+# Excercise 3.2
+# friendlist=["Faiz","Shoaib","Farwa"]
+# for x in friendlist:
+#     print("Hello "+x)
+# Excercise 3.3
+# car=["Honda", "Toyata","BMW"]
+# for car in car:
+#     print("I would like to own "+car)
+# Excercise 3.4
+# invites=["Mom","Dad","Brother"]
+# for invites in invites:
+#     print(invites+ " You are invited at the dinner")
+# Excercise 3.5
+# invites=["Mom","Dad","Brother"]
+# print(invites[0]+ " You are invited ")
+# print(invites[1]+ " You are invited ")
+# print(invites[2]+ " You are invited ")
+# print("Brother can not make it to dinner")
+# invites[2] = "Sister"
+# print(invites[0]+ " You are invited ")
+# print(invites[1]+ " You are invited ")
+# print(invites[2]+ " You are invited ")
+# Excercise 3.6
+# invites=["Mom","Dad","Brother"]
+# print(invites[0]+ " You are invited ")
+# print(invites[1]+ " You are invited ")
+# print(invites[2]+ " You are invited ")
+# print("Brother can not make it to dinner")
+# invites[2] = "Sister"
+# print(invites[0]+ " You are invited ")
+# print(invites[1]+ " You are invited ")
+# print(invites[2]+ " You are invited ")
+# print("I have found a bigger table")
+# invites.insert(0,"Shoaib")
+# invites.insert(2,"Farwa")
+# invites.append("Ashir")
+# for invites in invites:
+#     print(invites+ " You are invited")
+# Excercise 3.7
+# invites=["Mom","Dad","Brother"]
+# print(invites[0]+ " You are invited ")
+# print(invites[1]+ " You are invited ")
+# print(invites[2]+ " You are invited ")
+# print("Brother can not make it to dinner")
+# invites[2] = "Sister"
+# print(invites[0]+ " You are invited ")
+# print(invites[1]+ " You are invited ")
+# print(invites[2]+ " You are invited ")
+# print("I have found a bigger table")
+# invites.insert(0,"Shoaib")
+# invites.insert(2,"Farwa")
+# invites.append("Ashir")
+# print(invites[0]+ " You are invited ")
+# print(invites[1]+ " You are invited ")
+# print(invites[2]+ " You are invited ")
+# print(invites[3]+ " You are invited ")
+# print(invites[4]+ " You are invited ")
+# print(invites[5]+ " You are invited ")
+# print("Can only invite two people")
+# a=invites.pop()
+# print(a+ " Sorry could not invite you")
+# a=invites.pop()
+# print(a+ " Sorry could not invite you")
+# a=invites.pop()
+# print(a+ " Sorry could not invite you")
+# a=invites.pop()
+# print(a+ " Sorry could not invite you")
+# print(invites[0]+ " You are invited ")
+# print(invites[1]+ " You are invited ")
+# del invites[0]
+# print(invites)
+# del invites[0]
+# print(invites)
+# Excercise 3.8
+# cities=["Toronto","Karachi","Sydney","Perth","London"]
+# print(cities)
+# print(sorted(cities))
+# print(cities)
+# cities.reverse()
+# print(cities)
+# cities.reverse()
+# print(cities)
+# cities.sort()
+# print(cities)
+# Excercise 3.9
+# cities=["Toronto","Karachi","Sydney","Perth","London"]
+# print(len(cities))
